eba295e51562965714dcc7cc0425c6b7f0ddc82e
xy <2925243752@qq.com>
2021-07-26 18:05:57 +0800

    完善代码(补充不能整除的情况——作业讲解中提到)

---
+++ betterTask2.py
@@ -0,0 +1,75 @@
+
+#编码
+base='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+inputStr=input('请输入一个字符串：')
+outputStr=''
+binary='' #存储输入字符串对应的拼接二进制串
+for item in inputStr:
+    asciiNum=ord(item) #字符转换成ascii码
+    # print(asciiNum)
+    binStr=bin(asciiNum) #acill码转换成二进制
+    # print(binStr)
+    binStr=binStr[2:]
+    if len(binStr)<8:
+        zeroCount=8-len(binStr) #二进制串少于八位补零个数
+        for i in range(zeroCount):
+            binStr = '0'+binStr #补零后的八位二进制串
+        # print(binStr)
+        binary += binStr #将每个字符的八位二进制串拼接起来
+    else:
+        binary += binStr
+# print(binary)
+while len(binary)%6 != 0:
+    binary += '0' #确保能够取六的整数倍
+# print(binary)
+for j in range(0,len(binary),6):
+    index=int('0b'+binary[j:j+6],base=2) #取六位二进制串转十进制作为索引
+    # print(index)
+    outputStr += base[index:index+1] #索引查base编码
+# 在base64编码后加=，代表是补全过的
+if len(inputStr)%3==1:
+    outputStr += '=='
+elif len(inputStr)%3==2:
+    outputStr += '='
+else:
+    pass
+print(outputStr)
+
+#解码
+# base='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+# baseList=list(base)
+# # print(baseList)
+# inputStr=input('请输入一个Base64编码：')
+# outputStr=''
+# binary=''
+# for item in inputStr:
+#     if item != '=': #去除用户输入的base码后面多余的等号'='
+#         index=baseList.index(item)
+#         # print(index)
+#         binStr=bin(index)
+#         # print(binStr)
+#         binStr=binStr[2:]
+#         if len(binStr)<6:
+#             zeroCount=6-len(binStr) 
+#             for i in range(zeroCount):
+#                 binStr = '0'+binStr
+#             # print(binStr)
+#             binary += binStr 
+#         else:
+#             binary += binStr
+#     else:
+#         pass
+# # print(binary)
+# for j in range(0,len(binary),8):
+#     asciiNum=int('0b'+binary[j:j+8],base=2)
+#     # print(asciiNum)
+#     str=chr(asciiNum)
+#     # print(str)
+#     outputStr += str
+# print(outputStr)
+
+
+#测试准确性
+# import base64
+# print(base64.b64encode(b'Mance'))
+# print(base64.b64decode('TWE='))
